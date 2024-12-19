1015b638f7a8c28580782cc087047846e61327df
Debasis_sahu <sahurocky524@gmail.com>
2024-12-19 17:07:36 +0530

    made the home page responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import Home from './pages/home/Home'
 import Service from './pages/service/Service'
 import Contact from './pages/contact/Contact'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Background from './components/background/Background'
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/service" element={<Service />} />
-      </Routes>
-    </Router>
+    <>
+      <Background />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/service" element={<Service />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
