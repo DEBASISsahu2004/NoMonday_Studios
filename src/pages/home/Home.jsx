a2278d8f5ca8a58d774519be542342d17c9ab125
Debasis_sahu <sahurocky524@gmail.com>
2024-12-19 17:50:53 +0530

    corrected the css of link

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,11 +9,11 @@ const Home = () => {
       <div className={styles.logoContainer}>
       </div>
 
-      <a href="/service" className={styles.serviceButton}>Services<img src={RightArrow} alt="right arrow" /></a>
+    <a className={styles.serviceLink} href="/service"><button>Services<img src={RightArrow} alt="right arrow" /></button></a>
 
       <div className={styles.contentContainer}>
         <p className={styles.title}>Your brand deserves more, time for a revolution <span>!</span></p>
-        <a href="/contact" className={styles.contactButton}>CONTACT US</a>
+        <a href="/contact" className={styles.contactLink}>CONTACT US</a>
       </div>
 
     </div>
